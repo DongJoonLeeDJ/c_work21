3c02b4a4abf7402799f0ca39e511a10d1d464cb0
DongJoonLeeDJ <ldjgood12@gmail.com>
2021-02-22 12:08:07 +0900

    20210222

---
+++ 20210222/ex03.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+    int ch;
+    // wt -> 없으면 만들어서 보여줌
+    // rt -> 없으면 파일 못 열어줌
+    FILE *fp = fopen("aaa.txt","rt");
+    if(fp == NULL){
+        puts("그런파일 읽을수 없음");
+        return -1;
+    }
+
+    for(int i =0; i<3 ; i++){
+        ch = fgetc(fp);
+        printf("ch = %c \n",ch);
+    }
+    fclose(fp);
+    return 0;
+}
---
+++ 20210222/ex06.c
@@ -0,0 +1,37 @@
+/*
+프로그램상에서 mystory.txt 라는 이름의 파일을 생성하여
+본인의 이름,주민번호,전화번호를 저장하는 
+프로그램을 작성하자
+단저장의 형태는 다음과 같아야한다.(#문자도 합계 저장해야한다.)
+#이름:윤성우
+#주민번호:900208-1012589
+#전화번호:010-9946-2222
+그리고 저장이 완료되면 메모장으로 확인이 가능해야 한다.
+*/
+#include <stdio.h>
+void main(){
+    FILE *fp = fopen("mystory.txt","wt");
+    if(fp == NULL){
+        printf("파일열기실패");
+    }
+
+    char a[40];
+    printf("이름 입력 하세요\n");
+    fgets(a,sizeof(a),stdin);
+    printf("a = %s",a);
+    fputs("#이름 :",fp);
+    fputs(a,fp);
+
+    printf("주민번호입력 하세요\n");
+    fgets(a,sizeof(a),stdin);
+    printf("a = %s",a);
+    fputs("#주민번호 :",fp);
+    fputs(a,fp);
+
+    printf("전화번호 입력 하세요\n");
+    fgets(a,sizeof(a),stdin);
+    printf("a = %s",a);
+    fputs("#전화번호 :",fp);
+    fputs(a,fp);
+    fclose(fp);
+}
